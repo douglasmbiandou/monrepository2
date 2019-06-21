4a3298f19dd6b0fcd32644ddaad9159e5d15d41e
Douglas MBIANDOU <douglas.mbiandou@objis.com>
2019-06-21 08:42:06 +0200

    Maj 2 fetchPerson(1)

--- mockitodemo/src/test/java/com/objis/formationtest/PersonServiceTest.java
+++ mockitodemo/src/test/java/com/objis/formationtest/PersonServiceTest.java
@@ -51,7 +51,7 @@ public class PersonServiceTest
         when( personDAO.fetchPerson( 1 ) ).thenReturn( null );
         boolean updated = personService.update( 1, "David" );
         assertFalse( updated );
-        verify( personDAO ).fetchPerson( 2 );
+        verify( personDAO ).fetchPerson( 1 );
         verifyZeroInteractions( personDAO );
         verifyNoMoreInteractions( personDAO );
     }
